4e81c0e89a9e513b0dfd04ad525c6b23a4ee7585
Sebastian Gallardo <sebastian.gallardo1@mail.udp.cl>
2024-06-14 09:35:54 -0400

    listas

--- src/main/java/com/sebastian/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/sebastian/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,10 +1,15 @@
 package com.sebastian.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.sebastian.curso.springboot.webapp.springboot_web.models.User;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -15,6 +20,15 @@ public class UserController {
     model.addAttribute("title", "Hola Mundo Spring boot");
     model.addAttribute("user", user);
     return "details";
+  }
+
+  @GetMapping("/list")
+  public String list(ModelMap model) {
+    List<User> users = new ArrayList<>();
+
+    model.addAttribute("users", users);
+    model.addAttribute("title", "Listado de usuarios");
+    return "list";
   }
 
 }

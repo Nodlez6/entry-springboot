3b0dd9a477f4991efee9a25c450aff60de3a9efe
Sebastian Gallardo <sebastian.gallardo1@mail.udp.cl>
2024-06-17 10:53:43 -0400

    Anotattion RequestParam

---
+++ src/main/java/com/sebastian/curso/springboot/webapp/springboot_web/controllers/RequestParamsController.java
@@ -0,0 +1,22 @@
+package com.sebastian.curso.springboot.webapp.springboot_web.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sebastian.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/api/params")
+public class RequestParamsController {
+
+  @GetMapping("/foo")
+  public ParamDto foo(@RequestParam(required = false, defaultValue = "Hola", name = "otro") String message) {
+    ParamDto param = new ParamDto();
+    param.setMessage(message);
+    return param;
+  }
+
+}

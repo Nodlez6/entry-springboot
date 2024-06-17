e5ae37be1b4a7808bf5cb183d6790a8194f15137
Sebastian Gallardo <sebastian.gallardo1@mail.udp.cl>
2024-06-17 12:02:55 -0400

    varios parametros

--- src/main/java/com/sebastian/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/sebastian/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sebastian.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +21,15 @@ public class PathVariableController {
     ParamDto param = new ParamDto();
     param.setMessage(message);
     return param;
+  }
+
+  @GetMapping("/mix/{message}/{id}")
+  public Map<String, Object> mixPathVar(@PathVariable() String message, @PathVariable Long id) {
+    Map<String, Object> json = new HashMap<>();
+    json.put("message", message);
+    json.put("id", id);
+
+    return json;
   }
 
 }
